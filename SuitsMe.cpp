3f1ccb181b82565758b1c473a17f58ba33a4ac8f
lihaojin <hli002@citymail.cuny.edu>
2018-07-31 19:05:45 -0400

    Loop added to main

--- SuitsMe.cpp
+++ SuitsMe.cpp
@@ -21,11 +21,28 @@ int main() {
 	Player players[4];
 	CardDeck deck;
 	deck.shuffleDeck();
+	bool winner = false;
 
 	for(int i=0; i<4; ++i)
 	{
 		deck.dealCards(players[i],7);
-		//players[i].show_hand();
+	}
+
+	while(winner == false)
+	{
+		int card_index;
+		players[0].show_hand();
+		players[1].show_hand();
+		players[2].show_hand();
+		cout<<"Which card would you like to pass? Enter the number."<<endl;
+		cin >>card_index;
+		card_index--;
+		players[0].passCard(players[1],card_index);
+		players[1].auto_pass(players[2]);
+		players[0].show_hand();
+		players[1].show_hand();
+		players[2].show_hand();
+		cout<<"\n";
 	}
 
 
