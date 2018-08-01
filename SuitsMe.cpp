7e4bc61d750f9d43f8d6aeb8b9e789ca1ba4646b
lihaojin <hli002@citymail.cuny.edu>
2018-08-01 00:19:17 -0400

    auto pass and auto discard added to main

--- SuitsMe.cpp
+++ SuitsMe.cpp
@@ -17,6 +17,26 @@
 
 using namespace std;
 
+int num_discard(vector<string> discard_pile)
+{
+	int pile_top = discard_pile.size() - 1
+	string top_face = discard_pile[pile_top].substr(1,2);
+	if(top_face == 2 || top_face ==3 || top_face == 4 || top_face ==5 || top_face == 6 || top_face == 7 top_face == 8 || top_face == 9)
+	{
+		return 1;
+	}
+	else if((discard_pile[pile_top].substr(0,1) == "S" || discard_pile[pile_top].substr(0,1) == "C") && discard_pile[pile_top].substr(1,2) == "A")
+	{
+		return 3;
+	}
+	else if((discard_pile[pile_top].substr(0,1) == "H" || discard_pile[pile_top].substr(0,1) == "D") && discard_pile[pile_top].substr(1,2) == "A")
+	{
+		return 5;
+	}
+	else return 2;
+}
+
+
 int main() {
 	Player players[4];
 	CardDeck deck;
@@ -32,17 +52,40 @@ int main() {
 	{
 		int card_index;
 		players[0].show_hand();
-		players[1].show_hand();
-		players[2].show_hand();
-		cout<<"Which card would you like to pass? Enter the number."<<endl;
+		cout<<"Which card would you like to pass? Enter the number:"<<endl;
 		cin >>card_index;
 		card_index--;
+
 		players[0].passCard(players[1],card_index);
 		players[1].auto_pass(players[2]);
+		players[2].auto_pass(players[3]);
+		players[3].auto_pass(players[0]);
+
+		int discard_index;
 		players[0].show_hand();
-		players[1].show_hand();
-		players[2].show_hand();
-		cout<<"\n";
+
+		for(int i=0; i<num_discard(deck.discardPile); ++i)
+		{
+			cout<<"Which card would you like to discard? Enter the number: "<<endl;
+			cin >>discard_index;
+			discard_index--;
+			player[0].discard(discard_index, deck.discardPile);
+		}
+
+		for(int i=0; i<num_discard(deck.discardPile); ++i)
+		{
+			player[1].auto_discard(deck.discardPile);
+		}
+
+		for(int i=0; i<num_discard(deck.discardPile); ++i)
+		{
+			player[2].auto_discard(deck.discardPile);
+		}
+
+		for(int i=0; i<num_discard(deck.discardPile); ++i)
+		{
+			player[3].auto_discard(deck.discardPile);
+		}
 	}
 
 
