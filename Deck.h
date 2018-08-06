f10e8aa906c94f218ce62adefc94d453e0da88c0
lihaojin <hli002@citymail.cuny.edu>
2018-08-06 16:04:54 -0400

    discard bugs fixed

--- Deck.h
+++ Deck.h
@@ -17,43 +17,53 @@
 class CardDeck
 {
 private:
-	std::string faces[13] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-	std::string suits[4] = {"H","D","C","S"};
-
+    std::string faces[13] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    std::string suits[4] = {"H","D","C","S"};
+    
 public:
-
-	std::vector<std::string> deck = generateDeck(faces,suits);
-	std::vector<std::string> discardPile;
-
-	std::vector<std::string> generateDeck(std::string faces[13],std::string suits[4])
-	{
-		std::vector<std::string> cards;
-		for(int i=0; i<13; ++i){
-			for(int j=0; j<4; ++j){
-				std::string card = suits[j] + faces[i];
-				cards.push_back(card);
-			}
-		}
-		return cards;
-	}
-
-	void shuffleDeck()
-	{
-		std::srand(std::time(0));
-		std::random_shuffle(deck.begin(),deck.end());
-	}
-
-	void dealCards(Player& P, int num_cards)
-	{
-		for(int i=0; i<num_cards; ++i)
-		{
-			std::string temp = deck[deck.size()-1];
-			P.hand.push_back(temp);
-			deck.pop_back();
-		}
-	}
+    
+    std::vector<std::string> deck = generateDeck(faces,suits);
+    //std::vector<std::string> discardPile;
+    
+    std::vector<std::string> generateDeck(std::string faces[13],std::string suits[4])
+    {
+        std::vector<std::string> cards;
+        for(int i=0; i<13; ++i){
+            for(int j=0; j<4; ++j){
+                std::string card = suits[j] + faces[i];
+                cards.push_back(card);
+            }
+        }
+        return cards;
+    }
+    
+    void shuffleDeck()
+    {
+        std::srand(std::time(0));
+        std::random_shuffle(deck.begin(),deck.end());
+    }
+    
+    void dealCards(Player& P, int num_cards)
+    {
+        for(int i=0; i<num_cards; ++i)
+        {
+            std::string temp = deck[deck.size()-1];
+            P.hand.push_back(temp);
+            deck.pop_back();
+        }
+    }
+    
+    void show_discard(std::vector<std::string> &discardPile)
+    {
+        for(int i=0; i<discardPile.size(); ++i)
+        {
+            std::cout<<"("<<i+1<<") "<<discardPile[i]<<" ";
+        }
+        std::cout<<"\n";
+    }
 };
 
 
 
 #endif /* DECK_H_ */
+
--- SuitsMe.cpp
+++ SuitsMe.cpp
@@ -19,130 +19,134 @@ using namespace std;
 
 int num_discard(vector<string> discard_pile)
 {
-	int pile_top = discard_pile.size() - 1;
-	string top_face = discard_pile[pile_top].substr(1,1);
-	if(top_face == "1" || top_face == "2" || top_face == "3" || top_face == "4" || top_face == "5" || top_face == "6" || top_face == "7" || top_face == "8" || top_face == "9")
-	{
-		return 1;
-	}
-	else if((discard_pile[pile_top].substr(0,1) == "S" || discard_pile[pile_top].substr(0,1) == "C") && top_face == "A")
-	{
-		return 3;
-	}
-	else if((discard_pile[pile_top].substr(0,1) == "H" || discard_pile[pile_top].substr(0,1) == "D") && top_face == "A")
-	{
-		return 5;
-	}
-	else return 2;
+    int pile_top = discard_pile.size() - 1;
+    string top_face = discard_pile[pile_top].substr(1,1);
+    if(top_face == "1" || top_face == "2" || top_face == "3" || top_face == "4" || top_face == "5" || top_face == "6" || top_face == "7" || top_face == "8" || top_face == "9")
+    {
+        return 1;
+    }
+    else if((discard_pile[pile_top].substr(0,1) == "S" || discard_pile[pile_top].substr(0,1) == "C") && top_face == "A")
+    {
+        return 3;
+    }
+    else if((discard_pile[pile_top].substr(0,1) == "H" || discard_pile[pile_top].substr(0,1) == "D") && top_face == "A")
+    {
+        return 5;
+    }
+    else return 2;
 }
 
 void win_check(bool &winner,Player players[4])
 {
-	for(int i=0; i<4; ++i)
-	{
-		int maxsuit_num =0;
-		string maxsuit = players[i].get_maxsuit();
-		for(int j=0; j<players[i].hand.size(); ++j)
-		{
-			if(players[i].hand[j].substr(0,1) == maxsuit) maxsuit_num++;
-		}
-		if(maxsuit_num == players[i].hand.size() && maxsuit_num > 0)
-		{
-			cout<< "Winner is player "<<i+1<<"!";
-			winner = true;
-			break;
-		}
-	}
+    for(int i=0; i<4; ++i)
+    {
+        int maxsuit_num =0;
+        string maxsuit = players[i].get_maxsuit();
+        for(int j=0; j<players[i].hand.size(); ++j)
+        {
+            if(players[i].hand[j].substr(0,1) == maxsuit) maxsuit_num++;
+        }
+        if(maxsuit_num == players[i].hand.size() && maxsuit_num > 0)
+        {
+            cout<< "Winner is player "<<i+1<<"!";
+            winner = true;
+            break;
+        }
+    }
 }
 
 int main() {
-	Player players[4];
-	CardDeck deck;
-	deck.shuffleDeck();
-	bool winner = false;
-
-	for(int i=0; i<4; ++i)
-	{
-		deck.dealCards(players[i],7);
-	}
-
-	while(1)
-	{
-		win_check(winner,players);
-		if(winner) break;
-//		for(int i=0; i<4; ++i)
-//		{
-//			cout<<"Player "<<i<<"min_suit: "<<players[i].get_minsuit()<<"\n";
-//			cout<<"Player "<<i<<"max_suit: "<<players[i].get_maxsuit()<<"\n";
-//			cout<<"\n \n";
-//		}
-
-		players[0].show_hand();
-		players[1].show_hand();
-		players[2].show_hand();
-		players[3].show_hand();
-
-		int card_index;
-		players[0].show_hand();
-		cout<<"Which card would you like to pass? Enter the number:"<<endl;
-		cin >>card_index;
-		card_index--;
-
-		players[0].passCard(players[1],card_index);
-		players[1].auto_pass(players[2]);
-
-		players[2].auto_pass(players[3]);
-		players[3].auto_pass(players[0]);
-
-		int discard_index;
-		players[0].show_hand();
-
-		if(deck.discardPile.size() == 0)
-		{
-			string temp = deck.deck[deck.deck.size()-1];
-			deck.discardPile.push_back(temp);
-			deck.deck.pop_back();
-		}
-
-		for(int i=0; i<num_discard(deck.discardPile); ++i)
-		{
-			cout<<"Which card would you like to discard? Enter the number: "<<endl;
-			cin >>discard_index;
-			discard_index--;
-			players[0].discard(discard_index, deck.discardPile);
-		}
-
-		for(int i=0; i<num_discard(deck.discardPile); ++i)
-		{
-			players[1].auto_discard(deck.discardPile);
-		}
-
-		for(int i=0; i<num_discard(deck.discardPile); ++i)
-		{
-			players[2].auto_discard(deck.discardPile);
-		}
-
-		for(int i=0; i<num_discard(deck.discardPile); ++i)
-		{
-			players[3].auto_discard(deck.discardPile);
-		}
-
-		if(deck.deck.size() == 0)
-		{
-			for(int i=0; i<deck.discardPile.size() - 2; ++i)
-			{
-				deck.deck.push_back(deck.discardPile[i]);
-				deck.discardPile.erase(deck.discardPile.begin() + i);
-			}
-		}
-		else
-		{
-			for(int i=0; i<4; ++i)
-			{
-				deck.dealCards(players[i],1);
-			}
-		}
-	}
-
-
+    Player players[4];
+    vector<string> discardPile;
+    CardDeck deck;
+    deck.shuffleDeck();
+    bool winner = false;
+    
+    for(int i=0; i<4; ++i)
+    {
+        deck.dealCards(players[i],7);
+    }
+    
+    while(1)
+    {
+        win_check(winner,players);
+        if(winner) break;
+        //        for(int i=0; i<4; ++i)
+        //        {
+        //            cout<<"Player "<<i<<"min_suit: "<<players[i].get_minsuit()<<"\n";
+        //            cout<<"Player "<<i<<"max_suit: "<<players[i].get_maxsuit()<<"\n";
+        //            cout<<"\n \n";
+        //        }
+        
+        for(int i=1; i<4; ++i)
+        {
+            cout<<"Player "<<i+1<<": ";
+            players[i].show_hand();
+        }
+        
+        int card_index;
+        players[0].show_hand();
+        cout<<"Which card would you like to pass? Enter the number:"<<endl;
+        cin >>card_index;
+        card_index--;
+        
+        players[0].passCard(players[1],card_index);
+        players[1].auto_pass(players[2]);
+        
+        players[2].auto_pass(players[3]);
+        players[3].auto_pass(players[0]);
+        
+        int discard_index;
+        players[0].show_hand();
+        
+        if(discardPile.size() == 0)
+        {
+            string temp = deck.deck[deck.deck.size()-1];
+            discardPile.push_back(temp);
+            deck.deck.pop_back();
+        }
+        
+        for(int i=0; i<num_discard(discardPile); ++i)
+        {
+            cout<<"Which card would you like to discard? Enter the number: "<<endl;
+            cin >>discard_index;
+            discard_index--;
+            players[0].discard(discard_index, discardPile);
+        }
+        deck.show_discard(discardPile);
+        for(int i=0; i<num_discard(discardPile); ++i)
+        {
+            players[1].auto_discard(discardPile);
+        }
+        
+        for(int i=0; i<num_discard(discardPile); ++i)
+        {
+            players[2].auto_discard(discardPile);
+        }
+        deck.show_discard(discardPile);
+        for(int i=0; i<num_discard(discardPile); ++i)
+        {
+            players[3].auto_discard(discardPile);
+        }
+        deck.show_discard(discardPile);
+        
+        if(deck.deck.size() == 0)
+        {
+            for(int i=0; i<discardPile.size() - 2; ++i)
+            {
+                deck.deck.push_back(discardPile[i]);
+                discardPile.erase(discardPile.begin() + i);
+            }
+        }
+        else
+        {
+            for(int i=0; i<4; ++i)
+            {
+                deck.dealCards(players[i],1);
+            }
+        }
+    }
+    
+    
 }
+
